03f76a2598135c7aeb8d2a569634f02a29753eb2
Bill Barman <bill@anantya.com>
2017-11-09 11:26:29 +0800

    cleanup

--- app/src/main/java/com/anantya/watchsensor/StatusFragment.java
+++ app/src/main/java/com/anantya/watchsensor/StatusFragment.java
@@ -16,14 +16,6 @@ import com.anantya.watchsensor.data.EventDataStatItem;
 import java.util.Locale;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link StatusFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link StatusFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class StatusFragment extends Fragment {
 
 
@@ -112,9 +104,6 @@ public class StatusFragment extends Fragment {
 
 
     public void setIsPowered(boolean value) {
-//        mTextViewUploadProcessing.setVisibility(value ? View.VISIBLE : View.GONE);
-//        mTextViewUploadDone.setVisibility(value ? View.VISIBLE : View.GONE);
-
         mTextViewPercentDone.setVisibility(value ? View.VISIBLE : View.GONE);
         mProgressBar.setVisibility(value ? View.VISIBLE : View.GONE);
         View view = getView();
@@ -130,18 +119,8 @@ public class StatusFragment extends Fragment {
             mTextViewUploadWaitTitle.setText(R.string.status_fragment_recorded);
         }
     }
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
